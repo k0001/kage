c4fdaef8936f6e96c7d0c764720804379f02d6b6
k0001 <gnuk0001@gmail.com>
2009-01-07 18:21:53 -0200

    kage/kage.hh added

---
+++ kage/include/kage/kage.hh
@@ -0,0 +1,27 @@
+/*
+ * This file is part of Kage.
+ * Copyright (C) 2008, Renzo Carbonara <gnuk0001 AT gmail DOT com>
+ *
+ * Kage is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Kage is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kage.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef KAGE_HH_
+#define KAGE_HH_
+
+#include "kage/globals.hh"
+#include "kage/application.hh"
+#include "kage/application_state.hh"
+#include "kage/ogre_application.hh"
+
+#endif // APPLICATION_HH_
