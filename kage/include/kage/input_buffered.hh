df6003199a73b3dfcdd000b3ac97758847a92a27
k0001 <gnuk0001@gmail.com>
2009-01-07 19:51:48 -0200

    kage::core::input::BufferedInputManager preview

---
+++ kage/include/kage/input_buffered.hh
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Kage.
+ * Copyright (C) 2008, Renzo Carbonara <gnuk0001 AT gmail DOT com>
+ *
+ * Kage is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Kage is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kage.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef INPUT_BUFFERED_HH_
+#define INPUT_BUFFERED_HH_
+
+#include "kage/globals.hh"
+
+namespace kage {
+namespace core {
+namespace input {
+
+
+class BufferedInputManager
+{
+    public:
+        virtual ~BufferedInputManager(void);
+
+        /* Input setup (attach keyboard, mouse, etc...) */
+        virtual bool setup(void) = 0;
+};
+
+
+} // namespace input
+} // namespace core
+} // namespace kage
+
+#endif // INPUT_BUFFERED_HH_
