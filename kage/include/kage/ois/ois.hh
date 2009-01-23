3f29e6ea0f0b22ed3ce55a9a3f0658186a175996
k0001 <gnuk0001@gmail.com>
2009-01-23 02:24:05 -0200

    some autotools fixes/improvements

---
+++ kage/include/kage/ois/ois.hh
@@ -0,0 +1,24 @@
+/*
+ * This file is part of Kage.
+ * Copyright (C) 2008, Renzo Carbonara <gnuk0001 AT gmail DOT com>
+ *
+ * Kage is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * Kage is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Kage.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef KAGE_OIS_HH_
+#define KAGE_OIS_HH_
+
+#include "kage/ois/input_system.hh"
+
+#endif // KAGE_OIS_HH_
